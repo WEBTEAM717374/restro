453f0654e9f0c841002c234d5437eef3ff313915
WEBTEAM717374 <115256894+WEBTEAM717374@users.noreply.github.com>
2022-12-16 09:34:25 +0530

    Add files via upload

---
+++ restro/admin/partials/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+           <div class="wrapper">
+               <p class="text-center">2022 All rights resereved, Restro ,Developed by GGR- <a href="">RESTRO</a></p>
+           </div>
+      </div>
+   </body>
+
+
+</html>
